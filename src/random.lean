15afdc75d002f7f109eead33a011aa5347dfd28d
crab <92041779+crabbo-rave@users.noreply.github.com>
2022-05-04 20:17:50 -0400

    Renamed cantor_theorem since it isnt cantor_theorem

--- src/random.lean
+++ src/random.lean
@@ -5,7 +5,7 @@ import set_relations
 open set
 open_locale cardinal
 
-theorem cantor_theorem {α : Type} (f : α → set α) : # α ≤ # (set α) :=
+theorem set_lt_powerset {α : Type} (f : α → set α) : # α ≤ # (set α) :=
 begin 
   apply mk_le_of_injective,
   exact singleton_injective,
